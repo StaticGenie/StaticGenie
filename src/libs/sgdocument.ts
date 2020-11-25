dee5227688d908d5f32008c7ef700183a50ee942
ScottyCoder <73856230+ScottyCoder@users.noreply.github.com>
2020-11-25 16:21:01 +0000

    forgot the license header

--- src/libs/sgdocument.ts
+++ src/libs/sgdocument.ts
@@ -1,3 +1,9 @@
+/* ====================================================================
+ * License: MIT
+ * Website: https://staticgenie.com
+ * Source: https://github.com/StaticGenie/StaticGenie
+==================================================================== */
+
 import * as yaml from "js-yaml";
 import * as markdown from "markdown-it";
 
